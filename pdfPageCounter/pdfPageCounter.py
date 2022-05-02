ced7365e6e138d1f3367c316a384e91c2efc8bb5
Marco Gonçalves <marcomatiaspg@gmail.com>
2022-05-02 02:48:01 +0100

    add prints

--- pdfPageCounter/pdfPageCounter.py
+++ pdfPageCounter/pdfPageCounter.py
@@ -1,14 +1,20 @@
 from PyPDF2 import PdfFileReader
 import os
 
+
+
+
 if __name__ == "__main__":
-    d = "print out"
+    d = input("A pasta chama-se: ")
     directory = os.listdir(d)
     pages = 0
     for pdf in directory:
         if pdf.endswith('.pdf'):
+            print(pdf)
             pages += PdfFileReader(open(d + '/' + pdf, 'rb')).getNumPages()
 
+    print("Directory has the following:")
     for pdf in directory:
         print(pdf)
     print("Total =", pages, "pages")
+    print("Total files =", len(directory))
